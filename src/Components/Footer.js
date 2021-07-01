1944f0a837827eb392485a3a121f52a8a7c4d50c
debosmithoney <65868616+debosmithoney@users.noreply.github.com>
2021-07-01 21:19:44 +0530

    Contact

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,132 +1,136 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 import {
-	RiFacebookFill,
-	RiInstagramLine,
-	RiLinkedinFill,
-	RiTwitterFill,
+  RiFacebookFill,
+  RiInstagramLine,
+  RiLinkedinFill,
+  RiTwitterFill,
 } from "react-icons/ri";
 
 const Container = styled.div`
-	padding: 80px 60px;
-	background: #000;
-	bottom: 0;
-	@media (max-width: 768px) {
-		padding: 40px 10px;
-	}
+  padding: 80px 60px;
+  background: #000;
+  bottom: 0;
+  @media (max-width: 768px) {
+    padding: 40px 10px;
+  }
 `;
 const Wrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	width: 100%;
-	margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  margin: 0 auto;
 `;
 const Column = styled.div`
-	display: flex;
-	flex-direction: column;
-	text-align: left;
-	margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin: 0 auto;
 
-	& h1 {
-		color: #fff;
-		font-size: 32px;
-		text-align: center;
-		width: 300px;
-		margin-bottom: 80px;
-	}
-	@media (max-width: 768px) {
-		text-align: center;
-	}
+  & h1 {
+    color: #fff;
+    font-size: 32px;
+    text-align: center;
+    width: 300px;
+    margin-bottom: 80px;
+  }
+  @media (max-width: 768px) {
+    text-align: center;
+  }
 `;
 
 const Row = styled.div`
-	display: grid;
-	grid-template-columns: repeat(4, 1fr);
-	grid-gap: 20px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
 
-	@media (max-width: 768px) {
-		grid-template-columns: repeat(2, 1fr);
-	}
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
-const Links = styled(Link)`
-	color: #fff;
-	margin-bottom: 20px;
-	font-size: 16px;
-	text-decoration: none;
-	align-items: center;
+const Links = styled.a`
+  color: #fff;
+  margin-bottom: 20px;
+  font-size: 16px;
+  text-decoration: none;
+  align-items: center;
+  cursor: pointer;
 
-	&:hover {
-		color: #6c63ff;
-		transition: 200ms ease-in;
-	}
+  &:hover {
+    color: #6c63ff;
+    transition: 200ms ease-in;
+  }
 `;
 const Title = styled.div`
-	color: #fff;
-	margin-bottom: 40px;
-	font-size: 24px;
-	font-weight: bold;
+  color: #fff;
+  margin-bottom: 40px;
+  font-size: 24px;
+  font-weight: bold;
 `;
 
 const Facebook = styled(RiFacebookFill)`
-	font-size: 16px;
-	&:hover {
-		color: #385898;
-	}
+  font-size: 16px;
+  &:hover {
+    color: #385898;
+  }
 `;
 const Instagram = styled(RiInstagramLine)`
-	&:hover {
-		color: #8a3ab9;
-	}
+  &:hover {
+    color: #8a3ab9;
+  }
 `;
-const Linkdin = styled(RiLinkedinFill)`
-	&:hover {
-		color: #0e76a8;
-	}
+const LinkedIn = styled(RiLinkedinFill)`
+  &:hover {
+    color: #0e76a8;
+  }
 `;
 const Twitter = styled(RiTwitterFill)`
-	&:hover {
-		color: #1da1f2;
-	}
+  &:hover {
+    color: #1da1f2;
+  }
 `;
 
 const Footer = () => {
-	return (
-		<Container>
-			<Wrapper>
-				<Row>
-					<Column style={{ gridColumn: "1/3" }}>
-						<h1>Let's get you a Better Internet Experience 🚀</h1>
-					</Column>
-					<Column>
-						<Title>Contact Us</Title>
-						<Links onClick={() => window.open("tel:1234567890", "_self")}>
-							+91 1234569870
-						</Links>
-						<Links onClick={() => window.open("mailto:any@any.com", "_self")}>
-							any@any.com
-						</Links>
-					</Column>
-					<Column>
-						<Title>Social</Title>
-						<Links>
-							<Facebook /> Facebook
-						</Links>
-						<Links>
-							<Instagram /> Instagram
-						</Links>
-						<Links>
-							<Linkdin /> Linkedin
-						</Links>
-						<Links>
-							<Twitter /> Twitter
-						</Links>
-					</Column>
-				</Row>
-			</Wrapper>
-		</Container>
-	);
+  return (
+    <Container>
+      <Wrapper>
+        <Row>
+          <Column style={{ gridColumn: "1/3" }}>
+            <h1>Let's get you a Better Internet Experience 🚀</h1>
+          </Column>
+          <Column>
+            <Title>Contact Us</Title>
+            <Links href="tel:+918788305490">+91 87883 05490</Links>
+            <Links href="mailto:support@freepostseo.com">
+              support@freepostseo.com
+            </Links>
+          </Column>
+          <Column>
+            <Title>Social</Title>
+            <Links href="https://www.facebook.com/freepostseo/" target="_blank">
+              <Facebook /> Facebook
+            </Links>
+            <Links
+              href="https://www.instagram.com/free.post.seo/"
+              target="_blank"
+            >
+              <Instagram /> Instagram
+            </Links>
+            <Links
+              href="https://www.linkedin.com/company/freepostseo/"
+              target="_blank"
+            >
+              <LinkedIn /> LinkedIn
+            </Links>
+            <Links href="https://twitter.com/freepostseo1/" target="_blank">
+              <Twitter /> Twitter
+            </Links>
+          </Column>
+        </Row>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Footer;
